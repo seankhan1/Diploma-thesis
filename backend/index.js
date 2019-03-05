2d6dbf609a8018af0002c9f84736a6b935139e90
seankhan1 <seankhan74@gmail.com>
2019-03-05 12:00:00 +0500

    port edited

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT  || 8080
+const port = process.env.PORT  || 8000
 
 app.get('/test',(req,res) => {
    res.send('Test endpoint')
