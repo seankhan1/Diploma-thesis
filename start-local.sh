62b41f45665fc167d56f3d16fd63505f3c8439c2
seankhan1 <seankhan74@gmail.com>
2019-03-05 12:00:00 +0500

    moved to typescript

--- start-local.sh
+++ start-local.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env sh
 
-cd frontend
-[ ! -d "./node_modules" ] && npm i
-npm run build
+# cd frontend
+# npm install -g pnpm
+# rm -rf package-lock.json
+# rm -fr node_moduls
+# [ ! -d "./node_modules" ] && pnpm i
+# pnpm run build
+# pnpm start
 
-cd ../backend
-[ ! -d ./"node_modules" ] && npm i
-sleep 5 && open http://localhost:3000 &
-npm start
+# cd ../backend
+# [ ! -d ./"node_modules" ] && npm i
+# sleep 5 && open http://localhost:3001 &
+# npm start
+
+docker-compose build
+
+docker-compose -f ./backend/docker-compose.yml -f ./backend/docker-compose.dev.yml  up -d --build
+
+docker-compose -f ./frontend/docker-compose.yml -f ./frontend/docker-compose.dev.yml  up -d --build
+
+sleep 20 && open http://localhost:3001 
